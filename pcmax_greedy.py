1afb12e07dca55495da4891baf5c9b19b95d0518
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-04-07 16:18:45 +0200

    Use underscore instead of hyphen in scripts' names
    because they cannot be importet with hyphen

--- pcmax_greedy.py
+++ pcmax_greedy.py
@@ -4,15 +4,25 @@ from customio import getInput, visualize
 from utils import create2dArray
 
 
-def main():
-  (processorsCount, processesCount, processesTimes) = getInput()
+def pcmaxGreedy(processorsCount, processesCount, processesTimes):
   result = create2dArray(processorsCount)
 
   for process in processesTimes:
     lowestLenProcessor = result.index(min(result, key=sum))
     result[lowestLenProcessor].append(process)
 
+  return result
+
+
+def main():
+  (processorsCount, processesCount, processesTimes) = getInput()
+
+  result = pcmaxGreedy(processorsCount, processesCount, processesTimes)
+  max_time = max([sum(times) for times in result])
+
   visualize(result)
+
+  print('Maximum processor time: ' + str(max_time))
 
 
 if __name__ == "__main__":
