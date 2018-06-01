cbc4bd14f6c0fe8ccb09671ce60aa0798e4edf05
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-06-01 13:08:50 +0200

    Generate 4 processes per processors and change max queue length to 40 in generate

--- pcmax_generate.py
+++ pcmax_generate.py
@@ -4,29 +4,36 @@ from random import randint
 from utils import create2dArray
 
 
-MAX_QUEUE_LEN = 20
+MAX_QUEUE_LEN = 40
 
 
 def main():
-  if (len(argv) < 2):
-    print('Provide length as the second argument')
+    if (len(argv) < 2):
+        print('Provide length as the second argument')
 
-  processorsCount = int(argv[1])
-  queues = create2dArray(processorsCount)
+    processorsCount = int(argv[1])
+    queues = create2dArray(processorsCount)
 
-  for queue in queues:
-    randProcess = randint(1, MAX_QUEUE_LEN - 1)
-    completingProcess = MAX_QUEUE_LEN - randProcess
-    queue.append(randProcess)
-    queue.append(completingProcess)
+    for queue in queues:
+        randProcess = randint(1, MAX_QUEUE_LEN - 3)
+        secondRandProcess = randint(1, MAX_QUEUE_LEN - 2 - randProcess)
+        thirdRandProcess = randint(
+            1, MAX_QUEUE_LEN - 1 - randProcess - secondRandProcess)
+        completingProcess = MAX_QUEUE_LEN - \
+            (randProcess + secondRandProcess + thirdRandProcess)
+        queue.append(randProcess)
+        queue.append(secondRandProcess)
+        queue.append(thirdRandProcess)
+        queue.append(completingProcess)
 
-  processesCount = sum([len(queue) for queue in queues])
+    processesCount = sum([len(queue) for queue in queues])
 
-  print(processorsCount)
-  print(processesCount)
-  for queue in queues:
-    for item in queue:
-      print(item)
+    print(processorsCount)
+    print(processesCount)
+    for queue in queues:
+        for item in queue:
+            print(item)
+
 
 if __name__ == "__main__":
-  main()
+    main()
