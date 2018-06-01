ea2c5c9dc0a93a69e257e210a3120570fceedf0d
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-06-01 13:08:22 +0200

    Display chart only when param set and fix indent in greedy

--- pcmax_greedy.py
+++ pcmax_greedy.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 from random import randint
-from customio import getInput, visualize
+from customio import getInput, getParams, visualize
 from utils import create2dArray
 
 
 def pcmaxGreedy(processorsCount, processesCount, processesTimes):
-  result = create2dArray(processorsCount)
+    result = create2dArray(processorsCount)
 
-  for process in processesTimes:
-    lowestLenProcessor = result.index(min(result, key=sum))
-    result[lowestLenProcessor].append(process)
+    for process in processesTimes:
+        lowestLenProcessor = result.index(min(result, key=sum))
+        result[lowestLenProcessor].append(process)
 
-  return result
+    return result
 
 
 def main():
-  (processorsCount, processesCount, processesTimes) = getInput()
+    (processorsCount, processesCount, processesTimes) = getInput()
+    _, should_display_chart = getParams()
 
-  result = pcmaxGreedy(processorsCount, processesCount, processesTimes)
-  max_time = max([sum(times) for times in result])
+    result = pcmaxGreedy(processorsCount, processesCount, processesTimes)
+    max_time = max([sum(times) for times in result])
 
-  visualize(result)
+    print(str(max_time))
 
-  print('Maximum processor time: ' + str(max_time))
+    if should_display_chart:
+        visualize(result)
 
 
 if __name__ == "__main__":
-  main()
+    main()
